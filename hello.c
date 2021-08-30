45fccf57e727fc8b2f9b02e3494a21264ee72aee
macyhowe <mhowe5@hsukers.unl.edu>
2021-08-30 20:00:36 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -3,7 +3,7 @@
  *         mhowe5@huskers.unl.edu
  * Date: 2021/08/24
  *
- * This program prints out my name 
+ * This program prints out my name
  *
  */
 #include <stdlib.h>
@@ -12,8 +12,8 @@
 int main(int argc, char **argv) {
 
   printf("Macy Howe!\n");
-
+  printf("I am a Math Major\n");
   return 0;
 }
 
-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
+
