68055eed03d18e733b402b52184885cac48c7ea6
macyhowe <mhowe5@hsukers.unl.edu>
2021-08-30 18:54:38 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Macy Howe
+ *         mhowe5@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * This program prints out my name 
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Macy Howe!\n");
+
+  return 0;
+}
